f7ed052790010e3bdaa0fb78f84ea9a0ea67ec37
pf <petr.fischer@me.com>
2024-03-15 04:28:45 +0100

    ignore XHR requests + automatically wait for the app to be alive again

--- src/kemal-watcher/version.cr
+++ src/kemal-watcher/version.cr
@@ -1,3 +1,3 @@
 module Kemal::Watcher
-  VERSION = "0.4.0"
+  VERSION = "0.4.2"
 end
--- src/kemal-watcher/watcher_handler.cr
+++ src/kemal-watcher/watcher_handler.cr
@@ -4,26 +4,45 @@ module Kemal
   # In future release the code below could change
   class WatcherHandler
     def call(context)
-      if context.response.headers["Content-Type"] == "text/html"
+      if context.response.headers["Content-Type"]? == "text/html" && context.request.headers["X-Requested-With"]? != "XMLHttpRequest"
         context.response.print <<-HTML
         <script type="text/javascript">
+        var kemal_watcher_reloading = false;
+
+        async function wait_fot_alive() {
+          try {
+            var response = await fetch("/");
+            if (response.ok) {
+              window.location.reload();
+            }
+          } catch {
+            setTimeout(() => { wait_fot_alive() }, 1000);
+          }
+        }
+
         if ('WebSocket' in window) {
           (() => {
             var protocol = window.location.protocol === 'http:' ? 'ws://' : 'wss://';
             var address = protocol + window.location.host + '/' + `#{WEBSOCKETPATH}`;
             var ws = new WebSocket(address);
             ws.onopen = () => {
-              console.log("Connected to kemal-watcher");
+              console.log("Kemal-watcher - connected");
             };
             ws.onmessage = (msg) => {
               if (msg.data == "reload") {
+                console.log("Kemal-watcher - reloading...");
+                kemal_watcher_reloading = true;
                 window.location.reload();
               }
             };
             ws.onclose = () => {
-              setTimeout(() => {
-                window.location.reload();
-              }, 2000);
+              if (!kemal_watcher_reloading) {
+                console.log("Kemal-watcher - reconnecting...");
+                setTimeout(() => {
+                  wait_fot_alive();
+                  //window.location.reload();
+                }, 2000);
+              }
             };
           })();
         }
